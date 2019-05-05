2134933fea8166dd58385734ba48bb9b83ce68a3
superfeddy <fedf0415@gmail.com>
2019-05-05 03:02:54 +0200

    setState changes wrapper.debug() output

--- test/Form.test.js
+++ test/Form.test.js
@@ -13,19 +13,20 @@ describe('<Form /> rendering', () => {
     it('should fetch user f. name and l. name', () => {
         const id = 5;
         const url = `https://jsonplaceholder.typicode.com/users/${id}`
-        const resp = {data: {name:"from mock"}};
+        const mockName = 'from mock'
+        const resp = {data: {name:mockName}};
         axios.get.mockImplementation(() => Promise.resolve(resp))
-        //console.log(wrapper.debug())
         wrapper.find('#cid').simulate('change', {target: { value: `${id}` }})
         wrapper.find('#formButtonAdd').simulate('click')
+        wrapper.setState({clientName:mockName})
+        expect(wrapper.contains(`${mockName}`)).toBe(true)
         // number of times axios.get was called
         expect(axios.get).toHaveBeenCalledTimes(1)
         // passed argument
         expect(axios.get).toHaveBeenCalledWith(url)
         // The first argument of the first call to the function was url
         expect(axios.get.mock.calls[0][0]).toBe(url);  
-        // validate data returned by mock
+        // validate data returned by mock resolve promise
         return axios.get.mock.results[0].value.then(resp => expect(resp.data).toEqual(resp.data))
-           
     });
 });
